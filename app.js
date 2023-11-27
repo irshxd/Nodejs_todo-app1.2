221031848222c98bcc751dc7b70c2e87e26d24a4
irshxd <adildxi10@gmail.com>
2023-11-27 07:23:26 +0530

    origin in app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin:[],
+    origin:'http://localhost:5173',
     methods:['GET','POST','DELETE','PUT']
 }));
 // routes
