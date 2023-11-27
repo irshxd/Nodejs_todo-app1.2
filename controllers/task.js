5b3ffb1735bd67b642105232794fb23afe874f90
irshxd <adildxi10@gmail.com>
2023-11-28 02:31:40 +0530

    task changes in both route and controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,85 +1,71 @@
-import { Router } from "express"; 
+import ErrorHandler from "../middlewares/error.js";
 import { Task } from "../models/task.js";
 
-
 export const newTask = async (req, res, next) => {
-    try {
-      const { title, description } = req.body;
-      
-      // Assuming Task is your Mongoose model for tasks
-      const task = await Task.create({
-        title,
-        description,
-        user: req.user, // Assuming you have a user object attached to the request
-      });
+  try {
+    const { title, description } = req.body;
 
-      res.status(201).json({
-        success: true,
-        message: "Task added successfully",
-        task: task, // You can send back the created task if needed
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: "Internal Server Error",
-      });
-    }
-  };
-
-  export const myNewTask= async (req,res,next)=>{
-    try {
-    const userId  = req.user._id;
-    const tasks = await Task.find({user:userId});
+    await Task.create({
+      title,
+      description,
+      user: req.user,
+    });
 
     res.status(201).json({
-        success:true,
-        tasks,
-    })
-  }  catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
+      success: true,
+      message: "Task added Successfully",
     });
+  } catch (error) {
+    next(error);
   }
-}
-export const updateTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const task = await Task.findById(id);
-
-        if (!task) {
-            return res.status(404).json({
-                success: false,
-                message: 'Task not found',
-            });
-        }
-
-        task.isCompleted = !task.isCompleted;
-        await task.save();
-
-        res.status(200).json({
-            success: true,
-            message: 'Task updated',
-        });
-    } catch (error) {
-        console.error('Error updating task:', error);
-        res.status(500).json({
-            success: false,
-            message: 'Internal Server Error',
-        });
-    }
 };
 
+export const getMyTask = async (req, res, next) => {
+  try {
+    const userid = req.user._id;
 
-export const deleteTask= async(req,res)=>{
-    const {id }= req.params;
-    const task = await Task.findById(id);
-    task.isCompleted =!task.isCompleted;
+    const tasks = await Task.find({ user: userid });
+
+    res.status(200).json({
+      success: true,
+      tasks,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return next(new ErrorHandler("Task not found", 404));
+
+    task.isCompleted = !task.isCompleted;
+    await task.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Task Updated!",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return next(new ErrorHandler("Task not found", 404));
     await task.deleteOne();
-    res.status(201).json({
-        success:true,
-        message:"Task deleted"
-    })
-}
+
+    res.status(200).json({
+      message: "Task Deleted!",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
   
